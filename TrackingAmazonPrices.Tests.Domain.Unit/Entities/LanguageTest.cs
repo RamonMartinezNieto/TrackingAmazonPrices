5bf4cefe5c2670eb4f7b217fc5d44a7725c79345
RamónMN <ramon.martinez.nieto@gmail.com>
2023-09-14 20:27:12 +0200

    Add Language Tests

---
+++ TrackingAmazonPrices.Tests.Domain.Unit/Entities/LanguageTest.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using TrackingAmazonPrices.Domain.Entities;
+using TrackingAmazonPrices.Domain.Enums;
+using Xunit;
+
+namespace TrackingAmazonPrices.Tests.Domain.Unit.Entities;
+
+public class LanguageTest
+{
+    [Fact]
+    public void InstantiateLanguage_ReturnEngliss_VoidLanguage()
+    {
+        Language language = new();
+
+        language.LanguageCode.Should().Be(LanguageType.English);
+    }
+
+    [Fact]
+    public void InstantiateLanguage_ReturnSpanish_TypeConstructor()
+    {
+        Language language = new(LanguageType.Spanish);
+
+        language.LanguageCode.Should().Be(LanguageType.Spanish);
+    }
+
+    [Fact]
+    public void InstantiateLanguage_ReturnSpanish_StringConstructorEn()
+    {
+        Language language = new("en");
+
+        language.LanguageCode.Should().Be(LanguageType.English);
+    }
+
+    [Fact]
+    public void InstantiateLanguage_ReturnSpanish_StringConstructorEs()
+    {
+        Language language = new("es");
+
+        language.LanguageCode.Should().Be(LanguageType.English);
+    }
+}
