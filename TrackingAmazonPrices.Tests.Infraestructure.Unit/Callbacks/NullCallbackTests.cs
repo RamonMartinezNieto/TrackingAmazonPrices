053d6c5e5d6e01e53f2eaf6f8ef5490cbbe706ad
RamónMN <ramon.martinez.nieto@gmail.com>
2023-09-17 20:58:16 +0200

    add tests NullCallback

---
+++ TrackingAmazonPrices.Tests.Infraestructure.Unit/Callbacks/NullCallbackTests.cs
@@ -0,0 +1,24 @@
+﻿using TrackingAmazonPrices.Infraestructure.Callbacks;
+
+namespace TrackingAmazonPrices.Tests.Infraestructure.Unit.Callbacks;
+
+public class NullCallbackTests
+{
+    private readonly NullCallback _sut;
+
+    public NullCallbackTests()
+    {
+        _sut = new NullCallback();
+    }
+
+    [Fact]
+    public async Task ExecuteAsync_Always_ReturnsFalse()
+    {
+        var objectMessage = new object();
+        var dataCallback = "someData";
+
+        var result = await _sut.ExecuteAsync(objectMessage, dataCallback);
+
+        result.Should().BeFalse();
+    }
+}
