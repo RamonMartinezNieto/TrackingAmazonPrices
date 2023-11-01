1adeb9ff6405a141a03acfda5e7bd79bfbd3595b
RamónMN <ramon.martinez.nieto@gmail.com>
2023-11-01 20:36:51 +0100

    All things runs fine! Congrats Ramon :), now artifact deployed when test passed and Coverage is only collect when test passed to, i'm happy :)

--- TrackingAmazonPrices.Tests.Shared.Logging.Unit/LoggerConfigurationExtensionsTests.cs
+++ TrackingAmazonPrices.Tests.Shared.Logging.Unit/LoggerConfigurationExtensionsTests.cs
@@ -9,13 +9,6 @@ namespace TrackingAmazonPrices.Tests.Shared.Logging.Unit;
 
 public class LoggerConfigurationExtensionsTests
 {
-
-    [Fact]
-    public void Fail_Test() 
-    {
-        true.Should().BeFalse();
-    }
-
     [Fact]
     public void AddConsoleLogger_Enabled_AddsConsoleLoggerInformationLevel()
     {
